0d29dbdcf21a597b3bac57f2a7dcd5f1bfd13ece
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-03 16:38:21 -0400

    did some clean up

--- spinner2.js
+++ spinner2.js
@@ -1,10 +1,7 @@
-//process.stdout.write('hello from spinner1.js... \rheyyy\n');
 const spinner = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|']
-//['\r|', '\r/', '\r-', '\r\\', '\r|', '\r/', '\r-', '\r\\', '\r|']
 let time = 100;
 for (const spin of spinner) {
   setTimeout(() => {
-    //console.log("spin:", spin)
     process.stdout.write(`\r${spin}   `);
   }, time)
   time += 200
